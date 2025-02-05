fa016441ef88b6407d8192b6091d4737eea93d22
estherbg85 <estherbonillagirol85@gmail.com>
2025-02-05 10:23:06 +0100

    create repository

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './styles/App.scss'
+
+function App() {
+  return (
+    <div className="page">
+      <header>
+        <h1 className="header_title">Owen Wilson's "WOW"</h1>
+        <p className="header_border"></p>
+      </header>
+      <main
+
+      Escribe aquí tu HTML...
+    </div>
+  )
+}
+
+export default App
