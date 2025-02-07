aef0dfd6c3def6486156b0dd460eca92984064e6
estherbg85 <estherbonillagirol85@gmail.com>
2025-02-07 13:07:49 +0100

    css DetailPage

--- src/components/pages/DetailPage.jsx
+++ src/components/pages/DetailPage.jsx
@@ -1,5 +1,6 @@
 import {useParams, Link} from 'react-router';
-import ImgMovie from '../../images/120-1208201_available-in-two-formats-marco-de-foto-rollo.png'
+import imgmovie from '../../images/—Pngtree—a free director s chair_15014238 (2).png'
+
 
 function DetailPage({findMovie}) {
 
@@ -9,13 +10,14 @@ function DetailPage({findMovie}) {
 
     return (
         <section className="detail">
-            <img className="detail_img_background" src={ImgMovie}></img>
             <img className="detail_img" src={movie.poster} alt="imagen de pelicula"></img>
-            <div>
+            <div className="detail_colum">
                 <p className="detail_movie"><span className="detail_span">⭐</span>{movie.movie}<span className="detail_span">⭐</span></p>
                 <p className="detail_director">"{movie.director}"</p>
+                <img className="detail_director_chair" src={imgmovie}></img>
                 <p className="detail_full_line">{movie.full_line}</p>
-            <   a className="detail_audio">{movie.audio}</a>
+            <a href={movie.audio} download="NuevoNombreAudio.mp3"target="_blank" 
+  rel="noopener noreferrer" className="detail_audio">Escuchar Audio</a>
             </div>
             <Link to="/" className="btn">Volver</Link>
         </section>
