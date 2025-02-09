814751c5ab25a9ef80134d175b97c125dbd54a43
estherbg85 <estherbonillagirol85@gmail.com>
2025-02-09 20:26:03 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/modulo-3-evaluacion-final-estherbg85",
+  base: "/modulo-3-evaluacion-final-estherbg85/",
   plugins: [react()],
   server: {
     open: "/index.html",
